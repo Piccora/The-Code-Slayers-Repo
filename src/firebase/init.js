343c115826de380694d48ce5041240decb646452
bestfizzz <52324074+bestfizzz@users.noreply.github.com>
2023-08-30 21:39:40 +0700

    Update init.js

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -9,14 +9,6 @@ const firebaseConfig = {
     appId: import.meta.env.VITE_APP_ID,
 };
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBGWkxQybycYAgOtgVb2xX6LEv1mAv1kZw",
-//     authDomain: "the-code-slayers-project.firebaseapp.com",
-//     projectId: "the-code-slayers-project",
-//     storageBucket: "the-code-slayers-project.appspot.com",
-//     messagingSenderId: "108607517874",
-//     appId: "1:108607517874:web:de2081b700cebf325205ab"
-//   };
 
 const firebase = initializeApp(firebaseConfig,'Client2');
 
