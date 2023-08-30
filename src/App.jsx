3a30004abadbf43866a32794c7ee338bcbc4d39b
bestfizzz <minhhoangnguyen19122004@gmail.com>
2023-08-30 19:58:28 +0700

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import Layout from './components/ui/Layout'
-import { useState } from 'react';
+import Login from './pages/Login/Login'
+import Homepage from './pages/Homepage/Homepage'
+import { Routes, Route } from "react-router-dom";
+import { useEffect, useState } from 'react';
+// import {auth} from './firebase/Authentication';
 export default function App() {
-  const [currentUser, setCurrentUser] = useState(null)
+  useEffect(()=>{
+    console.log("Welcome")
+  },[])
   return (
     <>
-    <Layout>
-
-    </Layout>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Login />}>
+            <Route path="Login" element={<Homepage />} />
+            <Route path="*" element={<NoMatch />} />
+          </Route>
+        </Routes>
+      </Layout>
     </>
+  );
+}
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
   );
 }
--- src/firebase/Authentication.js
+++ src/firebase/Authentication.js
@@ -1,7 +1,7 @@
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
 import firebase from "./init.js";
 
-const auth = getAuth(firebase);
+export const auth = getAuth(firebase);
 console.log("test")
 
 export const createUser = (email, password) => {createUserWithEmailAndPassword(auth, email, password)
@@ -31,16 +31,4 @@ export const signIn = (email, password) => {signInWithEmailAndPassword(auth, ema
     const errorMessage = error.message;
   })};
 
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/auth.user
-      const uid = user.uid;
-      console.log(uid);
-      // ...
-    } else {
-      // User is signed out
-      // ...
-    }
-  });
   
