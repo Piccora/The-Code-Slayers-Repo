f960d497f80d750ff800338d62fb7614593ee5de
Piccora <tatuanminh31032004@gmail.com>
2023-08-31 10:06:36 +0700

    payment page

--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -4,9 +4,14 @@ export function Payment() {
   return (
     <div className="flex w-max flex-col gap-4 bg-green-600">
       <ButtonGroup color="yellow">
-        <Button>One</Button>
-        <Button>Two</Button>
-        <Button>Three</Button>
+        <Button>50 Coins for $5.00</Button>
+        <Button>100 Coins for $10.00</Button>
+        <Button>200 Coins for $20.00</Button>
+      </ButtonGroup>
+      <ButtonGroup color="yellow">
+        <Button>500 Coins for $50.00</Button>
+        <Button>1000 Coins for $100.00</Button>
+        <Button>5000 Coins for $500.00</Button>
       </ButtonGroup>
       </div>
   )
